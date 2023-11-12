4b71a2ce1ae527ac30eca9ce67b77aa8f511afbd
Douglas Candido <doug.candido2704@gmail.com>
2023-11-12 09:13:06 -0300

    Comentário de explicação removido de dentro da classe. A explicação está fora do código

--- BombaCombustivel/BombaCombustivel.java
+++ BombaCombustivel/BombaCombustivel.java
@@ -4,16 +4,6 @@ import java.util.Scanner;
  * 
  * @author Douglas
  *
- *         Vai cirar uma bomba de combustível.
- * 
- *         Onde, o cliente vai selecionar o tipo do combustível Gasolina Comum,
- *         Gasolina Aditivada, ou Etanol. Cada um tem seu valor em reais
- *         diferente.
- * 
- *         O cliente vai selecionar, e escolher, quantos litros.
- * 
- *         E ai precisa dar o resultado, do valor por litro. do combustível
- *         selecionado
  */
 public class BombaCombustivel {
 	public static void main(String[] args) {
